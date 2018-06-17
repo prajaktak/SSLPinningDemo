19d3c8a76bd3bb18b2b6eb09862a4af124e4724c
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-06-17 19:39:57 +0200

    Initial Commit

---
+++ SSLPinningDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSLPinningDemo
+//
+//  Created by Prajakta Kulkarni on 17/06/2018.
+//  Copyright © 2018 Prajakta Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
